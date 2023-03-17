09aa022b070af05cd685317c71f1a029265f4a1a
zhouxing9454 <zhouxing9454@gmail.com>
2023-03-17 14:53:57 +0800

    feature-2023-02-17-Factory_Method

---
+++ Create_mode/Factory_Method_test.go
@@ -0,0 +1,15 @@
+package Create_mode
+
+import "testing"
+
+func TestPay(t *testing.T) {
+	aPay := APayReq{}
+	if aPay.Pay() != "APay支付成功" {
+		t.Fatal("aPay error")
+	}
+
+	bPay := BPayReq{}
+	if bPay.Pay() != "BPay支付成功" {
+		t.Fatal("bPay error")
+	}
+}
