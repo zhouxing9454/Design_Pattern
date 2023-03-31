552b0d599acc1d08e8c2b1ef0d95b9971c877447
zhouxing9454 <zhouxing9454@gmail.com>
2023-03-31 12:19:54 +0800

    dev-2023-03-31-开闭原则

---
+++ 开闭原则.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+// AbstractBanker 抽象的银行业务员
+type AbstractBanker interface {
+	DoBusi() //抽象的处理业务接口
+}
+
+// SaveBanker 存款的业务员
+type SaveBanker struct {
+	//AbstractBanker
+}
+
+func (sb *SaveBanker) DoBusi() {
+	fmt.Println("进行了存款")
+}
+
+// 转账的业务员
+type TransferBanker struct {
+	//AbstractBanker
+}
+
+func (tb *TransferBanker) DoBusi() {
+	fmt.Println("进行了转账")
+}
+
+// PayBanker 支付的业务员
+type PayBanker struct {
+	//AbstractBanker
+}
+
+func (pb *PayBanker) DoBusi() {
+	fmt.Println("进行了支付")
+}
+
+func BankerBusiness(banker AbstractBanker) {
+	banker.DoBusi()
+}
+
+func main() {
+	//进行存款
+	sb := &SaveBanker{}
+	sb.DoBusi()
+
+	//进行转账
+	tb := &TransferBanker{}
+	tb.DoBusi()
+
+	//进行支付
+	pb := &PayBanker{}
+	pb.DoBusi()
+
+	//进行存款
+	BankerBusiness(&SaveBanker{})
+
+	//进行存款
+	BankerBusiness(&TransferBanker{})
+
+	//进行存款
+	BankerBusiness(&PayBanker{})
+}
